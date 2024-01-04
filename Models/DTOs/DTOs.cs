5a679f30dde512609d3ee3de21388fbeb8e87924
Ivádi Zsombor <izsombor@mitiszkad.lan>
2024-01-04 07:46:44 +0100

    DTOs created

--- Models/DTOs/DTOs.cs
+++ Models/DTOs/DTOs.cs
@@ -4,5 +4,14 @@
         public record EloadoDTO(Guid id, string EloadoNev, string EloadoNemzetiseg, string ZeneFajta, DateTime CreatedTime);
 
         public record CreatedEloadoDTO(string EloadoNev, string EloadtoNemzetiseg, string Zenefajta);
+
+        public record UpdateEloadoDTO(string EloadoNev, string EloadoNemzetiseg, string ZeneFajta);
+
+
+        public record ZeneDTO(Guid id, string SzamCim, string SzamMegjelenes, DateTime CreatedTime);
+
+        public record CreatedZeneDTO(string SzamCim, string SzamMegjelenes);
+
+        public record UpdateZeneDTO(string SzamCim, string SzamMegjelenes);
     }
 }
